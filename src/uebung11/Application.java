125dc090271299bf403c41d1e2c909a89600e105
Max <maxhildner@fastmail.com>
2016-05-09 16:39:35 +0200

    added gitignore

--- src/uebung11/Application.java
+++ src/uebung11/Application.java
@@ -1,13 +1,50 @@
 package uebung11;
 
+import java.io.IOException;
+
 public class Application {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		PorrMansDbWrapper dbWrapper = new PorrMansDbWrapper("dbFile.txt");
-		dbWrapper.saveWrapper(5, "Test");
-	//	System.out.println(dbWrapper.loadWrapper(5));
+		try
+		{
+			String dbPath = "/home/max/workspace/SOTE1/bin/uebung11/dbFile.txt";
+			
+			PorrMansDbWrapper dbWrapper = new PorrMansDbWrapper(dbPath);
+			System.out.println("Db under " + dbPath);
+			
+			dbWrapper.saveWrapper(5, "Test");
+			System.out.println("5 + Test");
+			
+			System.out.println(dbWrapper.loadWrapper(5));
+			System.out.println(dbWrapper.loadWrapper(123));
+			
+			
+		} catch (IOException ioEX){
+			
+			System.out.println(ioEX.getMessage());
+			
+		}
+		
+		try{
+			System.out.println(Application.halbieren(12));
+			System.out.println(Application.halbieren(13));
+		} catch (ArithmeticException arEx) {
+			System.out.println(arEx.getMessage());
+		}
+		
+	}
+	
+	public static int halbieren(int Zahl) throws ArithmeticException {
+	
+		if (Zahl % 2 != 0){
+			throw new ArithmeticException("Parameter war keine Gerade Zahl");
+		} else {
+			Zahl = Zahl / 2;
+		}
+		
+		return Zahl;
 	}
 
 }
--- src/uebung11/PorrMansDbWrapper.java
+++ src/uebung11/PorrMansDbWrapper.java
@@ -5,62 +5,95 @@ import java.io.IOException;
 
 public class PorrMansDbWrapper {
 
-	
-		private String filename;
-		private PoorMansDatabase db;
-	
-	
-	public PorrMansDbWrapper(String filename) {
+	private String filename;
+	private PoorMansDatabase db;
+
+	public PorrMansDbWrapper(String filename) throws IOException {
+
+		int count = 0;
+		boolean dbInitialized = false;
 		
-		try {
+		while (!dbInitialized) {
+
+			try {
+
+				this.filename = filename;
+				this.db = new PoorMansDatabase(filename);
+				dbInitialized = true;
+
+			} catch (IOException ioEx) {
+
+				System.out.println("! " + filename + " does not exist?");
+				count++;
 			
-			this.filename = filename;
-			this.db = new PoorMansDatabase(filename);
+			}
 			
-		} catch (IOException ioEx){
-			
-			System.out.println("! " + filename + " does not exist?");
-			
+			if (count > 20) {
+				IOException ioEx = new IOException("generation DB failed 20 times");
+				throw ioEx;
+			}
 		}
+		
 	}
-	
+
+	public void saveWrapper(int key, String value) throws IOException {
+		
+		int count = 0;
+		boolean dbWrite = false;
+		
+		while (!dbWrite) {
+
+			try {
+
+				db.save(key, value);
+				dbWrite = true;
+
+			} catch (IOException ioEx) {
+
+				System.out.println("Catched ioEx");
+				count++;
+			
+			}
+			
+			if (count > 5) {
+				IOException ioEx = new IOException("saving to DB failed 5 times");
+				throw ioEx;
+			}
+		}
+
+	}
+
+	public String loadWrapper(int key) throws IOException {
+
+		String reValue = "";
+		boolean dbLoad = false;
+		int count = 0;
+		
+		while (!dbLoad){
+		
+			try {
+				reValue = db.load(key);
+				dbLoad = true;
+				
+			} catch (IOException ioEx) {
+
+				System.out.println("Catched IOEx in load");
+				count++;
+			}
+
+			if (count > 5){
+				throw new IOException("load didn't work 5 Times");
+			}
+		}
+		
+		return reValue;
+	}
+
 	public String getFilename() {
 		return filename;
 	}
 
-
 	public void setFilename(String filename) {
 		this.filename = filename;
-	}
-	
-	public void saveWrapper(int key, String value){
-		
-		try {
-			
-			db.save(key, value);
-		
-		} catch (IOException ioEx) {
-			
-			System.out.println("Catched IOEX in Save");
-			
-		}
-		
-	}
-	
-	public String loadWrapper(int key){
-	
-		String reValue;
-		
-		try {
-			reValue = db.load(key);
-			return reValue;
-			
-		} catch (IOException ioEx){
-		
-		
-			System.out.println("Catched IOEx in load");
-			return null;
-		}	
-		
 	}
 }
